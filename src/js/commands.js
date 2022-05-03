5b294c99388fdc8426d7f730fabb9e631a7efffd
DuckWader <69596922+Shinobyc@users.noreply.github.com>
2022-05-03 22:48:37 +0300

    some commands

--- src/js/commands.js
+++ src/js/commands.js
@@ -22,8 +22,54 @@ function clear(args) {
     document.getElementById('wrapper').innerHTML = "";
 }
 
+//help command
 h = help;
 function help(args) {
-    block_log("Список команд: " + g + ", " + time + ", " + cls + ", " + h);
-    
+    block_log("Список команд: google(g), clear(cls), time(clock), help(h), duckduckgo(d), textcolor(setColor), promptcolor(setPromptColor), bgcolor(setBgColor)"); 
+}
+
+d = duckduckgo;
+function duckduckgo(args) {
+    if (args != undefined) {
+        search = args.replace(" ", "+")
+        window.open("https://www.duckduckgo.com/?q=" + search);
+    } else {
+        window.open("https://www.duckduckgo.com");
+    }  
+}
+
+
+textcolor = setColor
+function setColor(args) {
+    if (args != undefined) {
+        document.documentElement.style.setProperty("--text-color", args)
+        document.cookie = "textcolor=" + args
+    }
+}
+
+bgcolor = setBgColor
+function setBgColor(args) {
+    if (args != undefined) {
+        document.documentElement.style.setProperty("--main-bg-color", args)
+        document.cookie = "bgcolor=" + args
+    }
+}
+
+promptcolor = setPromptColor
+function setPromptColor(args) {
+    if (args != undefined) {
+        document.documentElement.style.setProperty("--prompt-bg-color", args)
+        document.cookie = "promptcolor=" + args
+    }
+}
+
+r = reddit
+function reddit(args) {
+    if (args != undefined) {
+        search = args.replace(" ", "")
+        window.open("https://www.reddit.com/r/" + search);
+    }
+    else {
+        window.open("https://www.reddit.com");
+    }
 }
