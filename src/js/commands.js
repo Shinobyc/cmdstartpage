bbe9d810d0feede297698e9ca1300890723fb03c
D.Wader <draninatrusoff6@gmail.com>
2022-05-04 22:53:47 +0300

    so many commands

--- src/js/commands.js
+++ src/js/commands.js
@@ -9,6 +9,41 @@ function google(args) {
     }
 }
 
+// Search in duckduckgo
+d = duckduckgo;
+function duckduckgo(args) {
+    if (args != undefined) {
+        search = args.replace(" ", "+")
+        window.open("https://www.duckduckgo.com/?q=" + search);
+    } else {
+        window.open("https://www.duckduckgo.com");
+    }  
+}
+
+// Search reddit
+r = reddit
+function reddit(args) {
+    if (args != undefined) {
+        search = args.replace(" ", "")
+        window.open("https://www.reddit.com/r/" + search);
+    }
+    else {
+        window.open("https://www.reddit.com");
+    }
+}
+
+//Github search(!)
+git = github
+function github(args) {
+    if (args != undefined) {
+        search = args.replace(" ", "")
+        window.open("https://www.github.com/search?q=" + search);
+    }
+    else {
+        window.open("https://www.github.com/");
+    }
+}
+
 // Display time
 time = clock;
 function clock(args) {
@@ -22,54 +57,25 @@ function clear(args) {
     document.getElementById('wrapper').innerHTML = "";
 }
 
-//help command
-h = help;
-function help(args) {
-    block_log("Список команд: google(g), clear(cls), time(clock), help(h), duckduckgo(d), textcolor(setColor), promptcolor(setPromptColor), bgcolor(setBgColor)"); 
-}
-
-d = duckduckgo;
-function duckduckgo(args) {
-    if (args != undefined) {
-        search = args.replace(" ", "+")
-        window.open("https://www.duckduckgo.com/?q=" + search);
-    } else {
-        window.open("https://www.duckduckgo.com");
-    }  
-}
-
-
-textcolor = setColor
-function setColor(args) {
-    if (args != undefined) {
-        document.documentElement.style.setProperty("--text-color", args)
-        document.cookie = "textcolor=" + args
-    }
-}
-
-bgcolor = setBgColor
-function setBgColor(args) {
-    if (args != undefined) {
-        document.documentElement.style.setProperty("--main-bg-color", args)
-        document.cookie = "bgcolor=" + args
-    }
-}
-
-promptcolor = setPromptColor
-function setPromptColor(args) {
-    if (args != undefined) {
-        document.documentElement.style.setProperty("--prompt-bg-color", args)
-        document.cookie = "promptcolor=" + args
-    }
-}
-
-r = reddit
-function reddit(args) {
-    if (args != undefined) {
-        search = args.replace(" ", "")
-        window.open("https://www.reddit.com/r/" + search);
+// Manga
+mg = manga
+function manga(args) {
+    if (args == " mangadex") {
+        window.open("https://www.mangadex.org");
     }
     else {
-        window.open("https://www.reddit.com");
+        if (args == " mangalib") {
+            window.open("https://www.mangalib.me");
+        }
+        else {
+            if (args == " readmanga") {
+                window.open("https://www.readmanga.me");
+            }
+            else {
+                if (args != undefined) {
+                    block_log("site not found");
+                }
+            }
+        }
     }
 }
